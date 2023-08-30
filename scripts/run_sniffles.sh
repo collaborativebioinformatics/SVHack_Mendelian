ec65dfc31d03dd2e4f0c8d7819db64a9953e923d
hoeps <wolfram.hoeps@embl.de>
2023-08-30 20:23:31 +0200

    Adding sniffles script

---
+++ scripts/run_sniffles.sh
@@ -0,0 +1,8 @@
+sniffles --input HG002.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --vcf HG002.vcf
+sniffles --input HG003.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --vcf HG003.vcf
+sniffles --input HG004.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --vcf HG004.vcf
+
+sniffles --input HG002.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --snf HG002.snf
+sniffles --input HG003.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --snf HG003.snf
+sniffles --input HG004.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam --snf HG004.snf
+
