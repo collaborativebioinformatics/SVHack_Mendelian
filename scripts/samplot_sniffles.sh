d5a938455320e8cb10c0b24588ab44f07f369471
hoeps <wolfram.hoeps@embl.de>
2023-09-01 12:50:21 +0200

    Also made changes to samplot

--- scripts/samplot_sniffles.sh
+++ scripts/samplot_sniffles.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 #we save all the violations in a file
-awk -F ',' '$6 == "YES" { print }' samplot_sniffles_input_hg002_3_4_complete.csv> input.tsv
+awk -F ',' '$6 == "YES" { print }' samplot_sniffles_input_hg002_3_4_complete.csv > input.tsv
 # change "," to tabs
 sed -i 's/,/\t/g' input.tsv
 # parsed archive with the needed information from the vcf merged
-archive=input.tsv
+archive="input.tsv"
 # obtain line by line of the archive
 while IFS= read -r line
 do
-  
+
   #save ID in order to use it as png
   ID=$(awk '{print $1}' <<< "$line")
   # save chromosome in a variable
@@ -21,15 +21,21 @@ do
   # save the type of the variant of interest
   type=$(awk '{print $5}' <<< "$line")
 
-  
+  let "end=end+1"
+  echo ${start}
+  echo ${end}
+  echo SUPPPdfdsfdsfds
+
   samplot plot \
-      -n HG002 HG002 HG002 \
+      -n HG002_son HG003_father HG004_mother \
       -b HG002.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam \
         HG003.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam \
         HG004.SequelII.merged_15kb_20kb.pbmm2.GRCh38.haplotag.10x.bam \
-      -o "$ID"_sniffles.png \
+      -o "sniffles/$ID"_sniffles.png \
       -c "$chrom" \
       -s "$start" \
       -e "$end" \
-      -t "$type"
-done < <("$archive")
+      -t "$type" \
+      --ignore_hp
+done < "$archive"
+
